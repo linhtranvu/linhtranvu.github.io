915ccf4fea857dbe4d5c4eb75a6119c7d161dbe3
Tran Vu Linh <linhtranvu@gmail.com>
2020-10-11 01:34:01 +0700

    dashboard-auth.js

---
+++ node-red/dashboard-auth.js
@@ -0,0 +1,85 @@
+/*
+  Change username and password to any username and password you want
+  You can make beautiful login form by changing htmlForm code, or use fs.readFileSync to read from external html.
+  In case you want to rewrite authentication and user 3rd party authentication system (oAuth, Twitter...), uncomment tokenValidate function and rewrite
+*/
+
+var username = "user"; //Username for Dashboard authentication
+var token = "123456"; //Password for Dashboard authentication
+var htmlForm = '<form >Username: <input name="username" type="text"><br>Password: <input name="token"  type="text"><br><input type="submit" value="Submit"></form>';
+
+function dashboardAuth(req, res, next) {
+  var req_username = req.query.username;
+  var req_token = req.query.token;
+
+  var url = req.originalUrl;
+  if (url[3] === '/' && url[4] !== undefined && url[4] !== '#' && url[4] !== '?') {
+    return next();
+  }
+
+  if(req_username == undefined || req_token == undefined){
+    res.writeHeader(200, {"Content-Type": "text/html"})
+    res.write(htmlForm);
+    return res.end();	  
+  }
+  
+  if(req_username == username && req_token == token){
+	  return next();
+  }else{
+      console.log("invalid token to access dashboard");
+      res.writeHeader(200, {"Content-Type": "text/html"})
+      res.write(htmlForm);
+      return res.end();	 
+      // return res.status(401).end();
+  }
+
+
+  /*
+  tokenValidate(username, token).then(function(result) {
+    if (result) {
+      return next();
+    } else {
+      console.log("invalid token to access dashboard");
+      return res.status(401).end();
+    }
+  });
+  */
+
+}
+
+
+/*
+var when = require("when");
+ var request = require("request");
+
+function tokenValidate (username, token) {
+  return when.promise(function(resolve) {
+    request.post(
+      {
+        url: "http://cloudred.cc/api/user/validate",
+        form: {
+          username: username,
+          token: token,
+        },
+      },
+      function(err, res, body) {
+        console.log(err, body);
+        try {
+          var result = JSON.parse(body)
+          if (result.success) {
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        } catch (e) {
+          console.log("parse res body:", body, "failed");
+          resolve(false);
+        }
+      }
+    )
+  });
+}
+*/
+
+
+module.exports = dashboardAuth;
