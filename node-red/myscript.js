a176c2cfdfe5b80ae030c942707402ce0d556053
Tran Vu Linh <linhtranvu@gmail.com>
2020-10-13 15:13:28 +0700

    admin

--- node-red/myscript.js
+++ node-red/myscript.js
@@ -1,10 +1,13 @@
 
  setTimeout(function(){ 
 
-  $("html").append('<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 0;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>');
-  
+
+  $("html").append('<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 0;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>');	  
   
 if($('.red-ui-deploy-button-groupt').length == 0) {
+	
+
+}else{
 	
   myAdminHtml = '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;bottom: 20px; right: 80px;z-index: 9999; ">';
   myAdminHtml += '<button id="btn-mobile-edit" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #21ba45;"><b>EDIT</b></button>';
@@ -13,7 +16,7 @@ if($('.red-ui-deploy-button-groupt').length == 0) {
   
   myAdminHtml += '</div>';
   $("html").append(myAdminHtml);	
-
+  
 }  
 
   
