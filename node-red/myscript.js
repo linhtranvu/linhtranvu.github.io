82c25358d30d4dfc1943e231b9cd856487c34927
Tran Vu Linh <linhtranvu@gmail.com>
2020-10-14 00:22:08 +0700

    red

--- node-red/myscript.js
+++ node-red/myscript.js
@@ -118,7 +118,7 @@ var checkExist = setInterval(function () {
 
   if ($('#nr-dashboard').length) {
 
-    $("html").append('<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: green; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 0;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>');
+    $("html").append('<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 0;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>');
 
     clearInterval(checkExist);
   }
