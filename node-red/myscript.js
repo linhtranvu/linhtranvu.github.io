877cb10b8e7d6b3cece9fd67807330066ab4b885
Tran Vu Linh <linhtranvu@gmail.com>
2020-10-14 00:46:25 +0700

    edit

--- node-red/myscript.js
+++ node-red/myscript.js
@@ -1,128 +1,59 @@
 var checkExist = setInterval(function () {
-  if ($('#red-ui-editor').length) {
+    if ($('#red-ui-editor').length) {
 
-    myAdminHtml = '<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 10px;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>';
+      myAdminHtml = '<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 10px;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>';
 
-    //Bottom menu		
-    myAdminHtml += '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;bottom: 20px; right: 70px;z-index: 9999; ">';
-    myAdminHtml += '<button id="btn-mobile-edit" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #21ba45;"><b>EDIT</b></button>';
-    myAdminHtml += '<button id="btn-mobile-nodelist" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;  "><b>+NODE</b></button>';
-    myAdminHtml += '<button id="btn-mobile-righlist" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;  "><b>INFO</b></button>';
-    myAdminHtml += '<button id="btn-mobile-refresh" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #d81b60; " ><b>F5</b></button>';
-
-
-    myAdminHtml += '</div>';
-    $("html").append(myAdminHtml);
-
-    //Top menu
-    myAdminHtml = '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 0px; left: 0px;z-index: 9999; ">';
-    myAdminHtml += '<button id="btn-mobile-more" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;"><b>MORE</b></button>';
-    myAdminHtml += '<button id="btn-mobile-undo" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;  "><b>UNDO</b></button>';
-
-    myAdminHtml += '</div>';
-    $("html").append(myAdminHtml);
-
-    //more menu
-    myAdminHtml = '<div class="mobile-more-menu controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 40px; left: 0px;z-index: 9999;display:none ">';
-    myAdminHtml += '<button id="btn-mobile-delete" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: red;margin-right:25px"><b>DELETE</b></button>';
-    myAdminHtml += '<button id="btn-mobile-cut" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;  "><b>CUT</b></button>';
-    myAdminHtml += '<button id="btn-mobile-copy" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;  "><b>COPY</b></button>';
-    myAdminHtml += '<button id="btn-mobile-paste" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;  "><b>PASTE</b></button>';
-    myAdminHtml += '<button id="btn-mobile-redo" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;  "><b>REDO</b></button>';
+      //Bottom menu		
+      myAdminHtml += '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;bottom: 20px; right: 70px;z-index: 9999; ">';
+      myAdminHtml += '<button id="btn-mobile-edit" onclick="mobile_edit()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #21ba45;"><b>EDIT</b></button>';
+      myAdminHtml += '<button id="btn-mobile-nodelist" onclick="mobile_nodelist()" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;  "><b>+NODE</b></button>';
+      myAdminHtml += '<button id="btn-mobile-righlist" onclick="mobile_righlist()" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;  "><b>INFO</b></button>';
+      myAdminHtml += '<button id="btn-mobile-refresh"  onclick="mobile_refresh()" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #d81b60; " ><b>F5</b></button>';
 
 
-    myAdminHtml += '</div>';
-    $("html").append(myAdminHtml);
+      myAdminHtml += '</div>';
+      $("html").append(myAdminHtml);
 
-    // RED.user.logout();
-    // RED.touch.radialMenu.active();
-    // console.log(RED.view.selection());
+      //Top menu
+      myAdminHtml = '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 0px; left: 0px;z-index: 9999; ">';
+      myAdminHtml += '<button id="btn-mobile-more"  onclick="mobile_more()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;"><b>MORE</b></button>';
+      myAdminHtml += '<button id="btn-mobile-undo"  onclick="mobile_undo()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;  "><b>UNDO</b></button>';
 
-    // console.log(RED);
+      myAdminHtml += '</div>';
+      $("html").append(myAdminHtml);
 
-
-
-    $("#btn-mobile-edit").click(function () {
-      // RED.actions.add("core:copy-selection-to-internal-clipboard",copySelection);
-      // RED.actions.add("core:cut-selection-to-internal-clipboard",function(){copySelection();deleteSelection();});
-      // RED.actions.add("core:paste-from-internal-clipboard",function(){importNodes(clipboard);});
-      // RED.actions.add("core:delete-selection",deleteSelection);
-      // RED.actions.add("core:edit-selected-node",editSelection);
-      // RED.actions.add("core:undo",RED.history.pop);
-      // RED.actions.add("core:redo",RED.history.redo);
-      // RED.actions.add("core:select-all-nodes",selectAll);
-      // RED.actions.add("core:zoom-in",zoomIn);
-      // RED.actions.add("core:zoom-out",zoomOut);
-      // RED.actions.add("core:zoom-reset",zoomZero);
-      // RED.actions.add("core:enable-selected-nodes", function() { setSelectedNodeState(false)});
-      // RED.actions.add("core:disable-selected-nodes", function() { setSelectedNodeState(true)});	  
-      // RED.editor.edit(RED.view.selection().nodes[0]);
-      RED.actions.invoke("core:edit-selected-node")
-
-    })
-
-    $("#btn-mobile-nodelist").click(function () {
-
-      RED.actions.invoke("core:toggle-palette")
-
-    })
-
-    $("#btn-mobile-righlist").click(function () {
-
-      RED.actions.invoke("core:toggle-sidebar")
-
-    })
-
-    $("#btn-mobile-undo").click(function () {
-
-      RED.actions.invoke("core:undo")
-
-    })
-
-    $("#btn-mobile-more").click(function () {
-
-      $(".mobile-more-menu").toggle();
-
-    })
-    $("#btn-mobile-refresh").click(function () {
-
-      window.location.reload(true)
-
-    })
+      //more menu
+      myAdminHtml = '<div class="mobile-more-menu controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 40px; left: 0px;z-index: 9999;display:none ">';
+      myAdminHtml += '<button id="btn-mobile-delete" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: red;margin-right:25px"  onclick="mobile_delete()" ><b>DELETE</b></button>';
+      myAdminHtml += '<button id="btn-mobile-cut" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;"  onclick="mobile_cut()" ><b>CUT</b></button>';
+      myAdminHtml += '<button id="btn-mobile-copy" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;"  onclick="mobile_copy()" ><b>COPY</b></button>';
+      myAdminHtml += '<button id="btn-mobile-paste" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;"  onclick="mobile_paste()" ><b>PASTE</b></button>';
+      myAdminHtml += '<button id="btn-mobile-redo" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;"  onclick="mobile_redo()" ><b>REDO</b></button>';
 
 
+      myAdminHtml += '</div>';
+      $("html").append(myAdminHtml);
+
+      // RED.user.logout();
+      // RED.touch.radialMenu.active();
+      // console.log(RED.view.selection());
+
+      // console.log(RED);
 
 
+      clearInterval(checkExist);
+    } //end if check existed
 
-    $("#btn-mobile-redo").click(function () {
-      RED.actions.invoke("core:redo")
-    })
+    //Check for dashboard
 
-    $("#btn-mobile-delete").click(function () {
-      RED.actions.invoke("core:delete-selection")
-    })
-    $("#btn-mobile-cut").click(function () {
-      RED.actions.invoke("core:cut-selection-to-internal-clipboard")
-    })
-    $("#btn-mobile-copy").click(function () {
-      RED.actions.invoke("core:copy-selection-to-internal-clipboard")
-    })
-    $("#btn-mobile-paste").click(function () {
-      RED.actions.invoke("core:paste-from-internal-clipboard")
-    })
+    if ($('#nr-dashboard').length) {
 
-    clearInterval(checkExist);
-  } //end if check existed
+      $("html").append('<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 0;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>');
 
-  //Check for dashboard
-
-  if ($('#nr-dashboard').length) {
-
-    $("html").append('<button onclick="apphome()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 0;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>');
-
-    clearInterval(checkExist);
-  }
-}, 500); // check every 500ms
+      clearInterval(checkExist);
+    }
+  },
+  500); // check every 500ms
 
 
 
@@ -138,6 +69,66 @@ function apphome() {
     home: "home"
   }));
 
+}
+
+function mobile_edit() {
+  RED.actions.invoke("core:edit-selected-node")
+}
+
+function mobile_nodelist() {
+
+  RED.actions.invoke("core:toggle-palette")
+
+}
+
+function mobile_righlist() {
+
+  RED.actions.invoke("core:toggle-sidebar")
+
+}
+
+function mobile_undo() {
+
+  RED.actions.invoke("core:undo")
+
+}
+
+function mobile_more() {
+
+  $(".mobile-more-menu").toggle();
+
+}
+
+function mobile_refresh() {
+
+  window.location.reload(true)
+
+}
+
+
+
+
+function mobile_redo() {
+
+
+  RED.actions.invoke("core:redo")
+}
+
+function mobile_delete() {
+
+  RED.actions.invoke("core:delete-selection")
+}
+
+function mobile_cut() {
+  RED.actions.invoke("core:cut-selection-to-internal-clipboard")
+}
+
+function mobile_copy() {
+  RED.actions.invoke("core:copy-selection-to-internal-clipboard")
+}
+
+function mobile_redo() {
+  RED.actions.invoke("core:paste-from-internal-clipboard")
 }
 
 function loginAdmin(username, password) {
