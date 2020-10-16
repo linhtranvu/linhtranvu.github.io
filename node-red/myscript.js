27d59eebad3a1bd7f062c91259048cca24487b98
Tran Vu Linh <linhtranvu@gmail.com>
2020-10-16 09:54:59 +0700

    test version

--- node-red/myscript.js
+++ node-red/myscript.js
@@ -1,5 +1,27 @@
+// Important API
+// RED.search.search("8f364e30.2b8a3") 
+// RED.editor.edit(a[0].node)
+
 var checkExist = setInterval(function () {
+
+
+    //ADMIN UI FOUND
     if ($('#red-ui-editor').length) {
+
+      myAdminHtml = `
+      <iframe id="iframe_dahsboard" src="http://localhost:1880/ui/?username=user&token=123456" style="z-index:1200;position: fixed;top: 40px;left: 0;" width="100%" height="100%"></iframe>
+      <button onclick="$('#iframe_dahsboard').toggle()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;position: fixed;top: 0px; left: 300px;z-index: 1050; "><i class="fa fa-window-close"></i></button>
+      `
+      $("html").append(myAdminHtml);
+
+
+
+
+      //Process CSS to get a Clean UI for Mobile
+
+      $("#red-ui-header-button-user").parent().hide();
+      $("#red-ui-header-button-deploy-icon").hide();
+      $(".red-ui-header-logo").hide();
 
       cssHtml = `<style>
         .ui-dialog .ui-dialog-titlebar {
@@ -10,13 +32,22 @@ var checkExist = setInterval(function () {
           width: ${screen.width-50}px;
           left: 70%
         }
+
+        .red-ui-editor .form-row input, .red-ui-editor .form-row div[contenteditable="true"], .red-ui-editor-dialog .form-row input, .red-ui-editor-dialog .form-row div[contenteditable="true"] {
+          width: 50%;
+        }
+        
+
+        
       </style>`
       $("html").append(cssHtml);
 
-      myAdminHtml = '<button onclick="mobile_admin_home()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 1000; padding: 10px;border-radius: 50%; position: fixed;bottom: 10px;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>';
+
+      //Admin home button
+      myAdminHtml = '<button onclick="mobile_admin_home()" class="md-raised md-button md-ink-ripple" type="button" aria-label="button" style="color:white; background-color: orange; z-index: 9999; padding: 10px;border-radius: 50%; position: fixed;bottom: 10px;right: 0;"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"></path></svg></button>';
 
       //Bottom menu		
-      myAdminHtml += '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;bottom: 20px; right: 70px;z-index: 9999; ">';
+      myAdminHtml += '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;bottom: 20px; right: 70px;z-index: 1000; ">';
       myAdminHtml += '<button id="btn-mobile-edit" onclick="mobile_edit()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #21ba45;"><i class="fa fa-edit"></i></button>';
       myAdminHtml += '<button onclick="mobile_search_node()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #9c27b0;"><i class="fa fa-search"></i></button>';
 
@@ -29,7 +60,7 @@ var checkExist = setInterval(function () {
       $("html").append(myAdminHtml);
 
       //Top menu
-      myAdminHtml = '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 0px; left: 0px;z-index: 9999; ">';
+      myAdminHtml = '<div class="controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 0px; left: 0px;z-index: 1000; ">';
       myAdminHtml += '<button id="btn-mobile-more"  onclick="mobile_more()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;"><b>MORE</b></button>'
       myAdminHtml += '<button id="btn-mobile-undo"  onclick="mobile_undo()"  class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;  "><i class="fa fa-undo"></i></button>';
       myAdminHtml += '<button id="btn-mobile-redo" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;"  onclick="mobile_redo()" ><i class="fa fa-repeat"></i></button>';
@@ -39,7 +70,7 @@ var checkExist = setInterval(function () {
       $("html").append(myAdminHtml);
 
       //More menu
-      myAdminHtml = '<div class="mobile-more-menu controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 40px; left: 0px;z-index: 9999;display:none ">';
+      myAdminHtml = '<div class="mobile-more-menu controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 40px; left: 0px;z-index: 1000;display:none ">';
       myAdminHtml += '<button id="btn-mobile-delete" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: red;margin-right:25px;border-radius:5px""  onclick="mobile_delete()" ><i class="fa fa-trash" style="font-size:30px"></i></button>';
       myAdminHtml += '<button id="btn-mobile-cut" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;border-radius:5px"  onclick="mobile_cut()" ><i class="fa fa-cut" style="font-size:30px"></i></button>';
       myAdminHtml += '<button id="btn-mobile-copy" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;;border-radius:5px"  onclick="mobile_copy()" ><i class="fa fa-copy" style="font-size:30px"></i></button>';
@@ -51,10 +82,10 @@ var checkExist = setInterval(function () {
 
 
       //Admin context app menu
-      contextAppHtml = '<div class="mobile_context_app" style="background-color: black;position: fixed;top: 0px; left: 0px;width:3000px;height:500px;display:none;z-index:500;opacity: 0.5; "></div>';
+      contextAppHtml = '<div class="mobile_context_app" style="background-color: black;position: fixed;top: 0px; left: 0px;width:3000px;height:5000px;display:none;z-index:1010;opacity: 0.8; "></div>';
 
-      contextAppHtml += '<button id=" btn-mobile-delete" class="mobile_context_app ui-button ui-widget ui-corner-all" style="color:white;background-color: red;border-radius:5px;position: fixed;top: 0px; right: 0px;display:none;z-index:9999"  onclick="apphome()" >QUIT APP</button>';
-      contextAppHtml += '<div class="mobile_context_app controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 160px; left: 10px;z-index: 9999;display:none ">';
+      contextAppHtml += '<button id=" btn-mobile-delete" class="mobile_context_app ui-button ui-widget ui-corner-all" style="color:white;background-color:red;position: fixed;top: 0px; right: 0px;display:none;z-index:1020;font-size:15px"  onclick="apphome()" ><i class="fa fa-power-off"></i>&nbsp;&nbsp;QUIT APP</button>';
+      contextAppHtml += '<div class="mobile_context_app controlgroup ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix" style="position: fixed;top: 160px; left: 10px;z-index: 1020;display:none ">';
       contextAppHtml += '<button class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;border-radius:5px"  onclick="mobile_mqtt()" >MQTT</button>';
       contextAppHtml += '<button class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #1976d2;border-radius:5px"  onclick="mobile_location_guide()" >LOCATION</button>';
       contextAppHtml += '<button class="ui-button ui-widget ui-corner-all" style="color:white;background-color: #31ccec;border-radius:5px"  onclick="mobile_voice()" >VOICE</button>';
@@ -67,7 +98,8 @@ var checkExist = setInterval(function () {
       <p > <b>To use location tracking, you need to do these steps:</b></p> 
       <ul>
         <li>Turn on Location setting on Phone</li>
-        <li >Create Nodes to receive location data. Below button create a default flows, APp call "HTTP IN" to send location data, then update to MQTT</li>
+        <li>Create Nodes to receive location data.Below button create a
+        default flows, App will call <span style="color:blue"> "${admin_url}location/lat=lat_data&lon=lon_data"</span> to send location data, then update to MQTT</li>
         <li>Use "Send location" to check if data sent in Debug node</li>
       </ul>
       <p>App use background location tracking, try to send location data even when running in background. However, there is no guarantee for this feature to work perfectly because of "app killing mechanism". <a href="https://dontkillmyapp.com/">Visit here to learn more</a></p>
@@ -79,6 +111,10 @@ var checkExist = setInterval(function () {
        onclick = "mobile_send_location()" > SEND LOCATION </button>        
       </div>`;
       $("html").append(myAdminHtml);
+
+      $('#iframe_dahsboard').on('load', function () {
+        loadDashboarIframe(2000);
+      });
 
 
 
@@ -120,9 +156,55 @@ function apphome() {
 
 //FUNCTION LIST FOR ADMIN
 
+function loadDashboarIframe(interval) {
+
+  var checkExist = setInterval(function () {
+
+      console.log('find dashboard')
+
+      //ADMIN UI FOUND
+      if ($("#iframe_dahsboard").contents().find('#nr-dashboard').length) {
+
+        $("#iframe_dahsboard").contents().find("md-card").css("border", "2px solid #120f4a").click(function (event) {
+
+          /*
+          let labelName = $(this).find("label").html();
+          console.log(labelName);
+          if (labelName == undefined) {
+            labelName = $(this).find(".label").html();
+          }
+          RED.actions.invoke("core:search");
+          $(".red-ui-searchBox-input").val(labelName)
+
+          // RED.actions.invoke("core:edit-selected-node");
+          $("#red-ui-search").css("z-index","9999");
+
+          var e = $.Event("keydown");
+          e.which = 13; //choose the one you want
+          e.keyCode = 13;
+          $(".red-ui-searchBox-input").trigger(e);
+          $(".red-ui-searchBox-form").submit();
+          */
+
+          let searchNode = RED.search.search($(this).attr("node-id"));
+          RED.editor.edit(searchNode[0].node);
+          setEditPanelLayout(1);
+
+        }) //End handle click on UI node
+
+        $("#iframe_dahsboard").contents().find("ui-card-panel").css("border", "1px solid red")
+
+        clearInterval(checkExist);
+      } //end if check existed
+
+    },
+    500); // check every 500ms	  
+}
+
 function mobile_location_guide() {
   $("#dialog").dialog({
-    width: screen.width
+    width: screen.width,
+    height: screen.height - 200,
   });
   $(".mobile_context_app").toggle();
 }
@@ -130,7 +212,7 @@ function mobile_location_guide() {
 function mobile_create_location_node() {
 
   RED.actions.invoke("core:show-import-dialog");
-  $("#red-ui-clipboard-dialog-import-text").val(`[{"id":"b3d47392.90a89","type":"ip","z":"b18b632d.3d3d7","name":"ip","https":false,"timeout":"5000","internalIPv4":false,"internalIPv6":false,"publicIPv4":true,"publicIPv6":false,"x":330,"y":100,"wires":[["d1cea111.5ffa2","d420f642.a1f178"]]},{"id":"75d3a8f5.175928","type":"inject","z":"b18b632d.3d3d7","name":"","topic":"","payload":"","payloadType":"date","repeat":"600","crontab":"","once":true,"onceDelay":"0.1","x":110,"y":100,"wires":[["b3d47392.90a89"]]}]`);
+  $("#red-ui-clipboard-dialog-import-text").val(`[{"id":"daf21f1.bb4216","type":"http in","z":"b18b632d.3d3d7","name":"Get Location","url":"/location","method":"get","upload":false,"swaggerDoc":"","x":810,"y":260,"wires":[["b668cfa6.11cc2","b821bdc5.79e3b8","864b8ed5.23895"]]},{"id":"864b8ed5.23895","type":"http response","z":"b18b632d.3d3d7","name":"","statusCode":"","headers":{"content-type":"text/html"},"x":1010,"y":240,"wires":[]},{"id":"b668cfa6.11cc2","type":"mqtt out","z":"b18b632d.3d3d7","name":"","topic":"phone/location","qos":"","retain":"","broker":"b5997a7e.f2121","x":1020,"y":320,"wires":[]},{"id":"b821bdc5.79e3b8","type":"debug","z":"b18b632d.3d3d7","name":"Debug Location","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"payload","targetType":"msg","x":990,"y":380,"wires":[]},{"id":"b5997a7e.f2121","type":"mqtt-broker","z":"","name":"MQTT Server","broker":"localhost","port":"1883","clientid":"","usetls":false,"compatmode":false,"keepalive":"60","cleansession":true,"birthTopic":"","birthQos":"0","birthPayload":"","closeTopic":"","closeQos":"0","closePayload":"","willTopic":"","willQos":"0","willPayload":""}]`);
 
   $("#red-ui-clipboard-dialog-ok").removeClass("ui-button-disabled ui-button-disabled ui-state-disabled");
   $("#red-ui-clipboard-dialog-ok").prop("disabled", false);
@@ -140,7 +222,9 @@ function mobile_create_location_node() {
 }
 
 function mobile_send_location() {
-
+  webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify({
+    location: "location"
+  }));
 }
 
 
@@ -149,7 +233,35 @@ function mobile_admin_home() {
 }
 
 function mobile_edit() {
-  RED.actions.invoke("core:edit-selected-node")
+  RED.actions.invoke("core:edit-selected-node");
+  setEditPanelLayout(0)
+
+}
+
+
+function setEditPanelLayout(deploy) {
+
+
+  console.log(deploy);
+  setTimeout(function () {
+    $("#red-ui-editor-stack").css("z-index", "9999")
+    $("#node-input-method").css("width", "50%")
+    $(".red-ui-typedInput-container").css("width", "37%")
+    $(".red-ui-tab-link-buttons,#node-input-lookup-group").css("right", "70px")
+    $("#red-ui-editor-stack").find("select").css("width", "50%")
+    $("#red-ui-editor-stack").find("input:text").css("width", "50%")
+    $("#red-ui-editor-stack").find("#node-input-format-editor").css("width", "80%")
+
+    if (deploy == 1) {
+      $("#node-dialog-ok").click(function () {
+
+        RED.actions.invoke("core:deploy-flows");
+        loadDashboarIframe(2000)
+
+      })
+    }
+  }, 1000)
+
 }
 
 function mobile_search_node() {
@@ -212,7 +324,10 @@ function mobile_paste() {
   RED.actions.invoke("core:paste-from-internal-clipboard")
 }
 
-function loginAdmin(username, password) {
+var admin_url = "";
+
+function loginAdmin(username, password, params_admin_url) {
+  admin_url = params_admin_url;
   var username = username;
   var password = password;
   setTimeout(function () {
