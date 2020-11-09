a5cbb7762a2b9adcaf7a6dc1242ee2882ae8adf8
Tran Vu Linh <linhtranvu@gmail.com>
2020-11-09 09:24:02 +0700

    red

--- node-red/version.js
+++ node-red/version.js
@@ -1,4 +1,4 @@
-var newest_version = '0.0.5'
-var newest_info = 'MQTT is added'
-var script_version = '0.0.5'
-var script_info = '0.0.5'
+var newest_version = '0.5.0'
+var newest_info = 'First official release, with background location tracking'
+var script_version = '0.5.0'
+var script_info = '0.5.0'
