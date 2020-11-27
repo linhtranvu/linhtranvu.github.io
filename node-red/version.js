4ff4cbb61d354cbdcdf57fa70e269e18cbb8f52d
Tran Vu Linh <linhtranvu@gmail.com>
2020-11-27 16:21:13 +0700

    version

--- node-red/version.js
+++ node-red/version.js
@@ -1,4 +1,4 @@
-var newest_version = '0.5.0'
-var newest_info = 'First official release, with background location tracking'
+var newest_version = '0.7.0'
+var newest_info = 'New release: Push from Node-RED, enhance UI and fix some issue'
 var script_version = '0.5.0'
 var script_info = '0.5.0'
