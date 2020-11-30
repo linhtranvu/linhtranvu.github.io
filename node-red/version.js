8efa74d1502dbd952d678d251c73c2a4b78b5f67
Tran Vu Linh <linhtranvu@gmail.com>
2020-11-30 08:29:31 +0700

    version

--- node-red/version.js
+++ node-red/version.js
@@ -1,4 +1,4 @@
-var newest_version = '0.7.0'
-var newest_info = 'New release: Push from Node-RED, enhance UI and fix some issue'
+var newest_version = '0.7.1'
+var newest_info = 'New release: Fix auto start for dashboard, testing feature of location, code optimize'
 var script_version = '0.5.0'
 var script_info = '0.5.0'
