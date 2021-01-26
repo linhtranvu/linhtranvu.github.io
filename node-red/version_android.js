f42485c849630c19280ad84f20e624112a6c4753
Tran Vu Linh <linhtranvu@gmail.com>
2021-01-26 08:47:24 +0700

    mqtt

--- node-red/version_android.js
+++ node-red/version_android.js
@@ -1,7 +1,7 @@
 jsonp_callback({
   newest_version: "1.0.3",
   newest_info:
-    "Voice command, MQTT, ZeroTier, UI fix",
+    "NEW: Voice command, MQTT, ZeroTier, UI fix.. Current features: WYSIWYG Editor, Push, Location tracking",
   script_version: "0.5.0",
   script_info: "Info about Editor",
 });
--- node-red/version_ios.js
+++ node-red/version_ios.js
@@ -1,7 +1,7 @@
 jsonp_callback({
-  newest_version: "1.0.3",
+  newest_version: "1.0.5",
   newest_info:
-    "Siri voice command, ZeroTier, WYSIWYG Editor",
+    "NEW: MQTT Client, UI Fixes... Current features: WYSIWYG Editor, Push, Location tracking, Siri voice command, ZeroTier",
   script_version: "0.5.0",
   script_info: "Info about Editor",
 });
