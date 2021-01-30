f0d8e3b8cb8c08e8ac3c50328ebb82977d1ae788
Tran Vu Linh <linhtranvu@gmail.com>
2021-01-30 18:31:56 +0700

    1.0.7

--- node-red/version_android.js
+++ node-red/version_android.js
@@ -1,7 +1,7 @@
 jsonp_callback({
   newest_version: "1.0.7",
   newest_info:
-    "Fix Voice command cannot send short message; More stable MQTT and better UI",
+    "Fix Voice command cannot send short message; More stable MQTT and better UI. Current features: WYSIWYG Editor, Push, Location tracking, Voice command, MQTT, ZeroTier",
   script_version: "0.5.0",
   script_info: "Info about Editor",
 });
--- node-red/version_ios.js
+++ node-red/version_ios.js
@@ -1,7 +1,7 @@
 jsonp_callback({
-  newest_version: "1.0.5",
+  newest_version: "1.0.7",
   newest_info:
-    "NEW: MQTT Client, UI Fixes... Current features: WYSIWYG Editor, Push, Location tracking, Siri voice command, ZeroTier",
+    "Update to fix Siri Voice command cannot send short message. NEW: Activate voice command with 'home' word; More stable MQTT and better UI... Current features: WYSIWYG Editor, Push, Location tracking, Siri voice command, ZeroTier, MQTT Client",
   script_version: "0.5.0",
   script_info: "Info about Editor",
 });
