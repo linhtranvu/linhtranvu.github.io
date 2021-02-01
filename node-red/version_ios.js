cfcb8f7d58e138bed025c8194f0978b3ef87619b
Tran Vu Linh <linhtranvu@gmail.com>
2021-02-01 09:47:46 +0700

    ios version_ios.js

--- node-red/version_ios.js
+++ node-red/version_ios.js
@@ -1,7 +1,7 @@
 jsonp_callback({
-  newest_version: "1.0.7",
+  newest_version: "1.0.8",
   newest_info:
-    "Update to fix Siri Voice command cannot send short message. NEW: Activate voice command with 'home' word; More stable MQTT and better UI... Current features: WYSIWYG Editor, Push, Location tracking, Siri voice command, ZeroTier, MQTT Client",
+    "NEW:Phone screen button, More stable voice command ... Current features: WYSIWYG Editor, Push, Location tracking, Siri voice command, Phone screen button, ZeroTier, MQTT Client",
   script_version: "0.5.0",
   script_info: "Info about Editor",
 });
