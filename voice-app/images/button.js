0aa846dbab386bd15bc2a51e2899dd4ada8a1277
Tran Vu Linh <linhtranvu@gmail.com>
2021-02-05 21:15:38 +0700

    js

---
+++ voice-app/images/button.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	
+$('.alert-primary').append(`<input type="text"  readonly="" id="linkInput"  value="${window.location.href}"><span  class="btn-copy btn btn-primary">Copy URL</span></div>`)
+
+	$('.btn-copy').click(function() {
+	  /* Get the text field */
+	  var copyText = document.getElementById("linkInput");
+
+	  /* Select the text field */
+	  copyText.select();
+	  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+	  /* Copy the text inside the text field */
+	  document.execCommand("copy");
+
+	  /* Alert the copied text */
+	  alert("Copied the text: " + copyText.value);
+	})           
+
+
+})
