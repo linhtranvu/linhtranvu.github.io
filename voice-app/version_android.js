b9b02b78fb806216ee77d9832e17b6bf4e6c8425
Tran Vu Linh <linhtranvu@gmail.com>
2021-02-05 22:15:06 +0700

    version_android.js

---
+++ voice-app/version_android.js
@@ -0,0 +1,8 @@
+jsonp_callback({
+  newest_version: "1.0.7",
+  newest_info:
+    "03 days free trial with PRO feature; Fix Voice command cannot send short message; More stable MQTT and better UI. Current features: WYSIWYG Editor, Push, Location tracking, Voice command, MQTT, ZeroTier",
+  script_version: "0.5.0",
+  script_info: "Info about Editor",
+});
+
---
+++ voice-app/version_ios.js
@@ -0,0 +1,8 @@
+jsonp_callback({
+  newest_version: "1.0.0",
+  newest_info:
+    "Current features: Voice command, HTTP server, MQTT server, Siri integration",
+  script_version: "0.5.0",
+  script_info: "Info about Editor",
+});
+
