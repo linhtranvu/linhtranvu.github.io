717b3950d6019e8829d6197ef26a4a87af596e3c
Tran Vu Linh <linhtranvu@gmail.com>
2021-02-09 21:28:53 +0700

    intril

--- node-red/editor_intro.js
+++ node-red/editor_intro.js
@@ -1,7 +1,19 @@
 
-$("#btn-editor-container").html(
-  `<button id="btn-editor-intro" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;"  onclick="editorIntro()" ><b>Editor Intro</b></button>`
-);
+
+
+var checkExistContainer = setInterval(function () {
+
+  var element_container = document.getElementById("btn-editor-container");
+  if (typeof element_container != "undefined" && element_container != null) {
+
+    clearInterval(checkExistContainer);
+    $("#btn-editor-container").html(
+      `<button id="btn-editor-intro" class="ui-button ui-widget ui-corner-all" style="color:white;background-color: orange;"  onclick="editorIntro()" ><b>Editor Intro</b></button>`
+    );   
+  }    
+
+}, 1000); // check every 500ms
+
 
 
 function editorIntro() {
