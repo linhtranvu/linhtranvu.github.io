61e7c413acd87fdf9468cfe666f1362fa2fa9994
Tran Vu Linh <linhtranvu@gmail.com>
2021-02-16 00:43:06 +0700

    version

--- node-red/version_android.js
+++ node-red/version_android.js
@@ -2,7 +2,7 @@ jsonp_callback({
   newest_version: "1.0.9",
   newest_info:
     "More stable MQTT and better UI. Current features: WYSIWYG Editor, Push, Location tracking, Voice command, MQTT, ZeroTier",
-  script_version: "0.8.0",
-  script_info: "New worldmap node. Update: gauge chart, better move with template, form",
+  script_version: "0.8.5",
+  script_info: "Fully support chart, better move scale",
 });
 
--- node-red/version_ios.js
+++ node-red/version_ios.js
@@ -2,7 +2,7 @@ jsonp_callback({
   newest_version: "1.0.8",
   newest_info:
     "NEW:Phone screen button, More stable voice command ... Current features: WYSIWYG Editor, Push, Location tracking, Siri voice command, Phone screen button, ZeroTier, MQTT Client",
-  script_version: "0.8.0",
-  script_info: "New worldmap node. Update: gauge chart, better move with template, form",
+  script_version: "0.8.5",
+  script_info: "Fully support chart, better move scale",
 });
 
